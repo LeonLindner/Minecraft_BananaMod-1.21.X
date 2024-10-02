4f757251dec571018d1fb1d20ac5dd1f55630908
LeonLindner <leonlucaslindner@hotmail.com>
2024-10-02 09:21:02 +0200

    Initial commit

--- src/main/java/net/leoncoding/bananamod/BananaMod.java
+++ src/main/java/net/leoncoding/bananamod/BananaMod.java
@@ -15,7 +15,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// test
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(BananaMod.MOD_ID)
 public class BananaMod {
