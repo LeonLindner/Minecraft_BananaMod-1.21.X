15f177cc8836b8b778eccb017067a6fdd46bbd5c
LeonLindner <leonlucaslindner@hotmail.com>
2024-10-02 13:12:13 +0200

    Update

--- src/main/java/net/leoncoding/bananamod/BananaMod.java
+++ src/main/java/net/leoncoding/bananamod/BananaMod.java
@@ -1,6 +1,8 @@
 package net.leoncoding.bananamod;
 
 import com.mojang.logging.LogUtils;
+import net.leoncoding.bananamod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -29,6 +31,8 @@ public class BananaMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -42,7 +46,9 @@ public class BananaMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BANANA);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/leoncoding/bananamod/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.leoncoding.bananamod.item;
+
+import net.leoncoding.bananamod.BananaMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS=
+            DeferredRegister.create(ForgeRegistries.ITEMS, BananaMod.MOD_ID);
+
+    public static final RegistryObject<Item> BANANA = ITEMS.register("banana",
+            () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
